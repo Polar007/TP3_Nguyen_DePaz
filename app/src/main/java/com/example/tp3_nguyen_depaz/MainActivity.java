3d157fe74eff557ee614d4f6327d5beb2685f8a7
samuel <samueldpz@gmail.com>
2021-10-12 15:15:11 +0200

    first commit

---
+++ app/src/main/java/com/example/tp3_nguyen_depaz/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tp3_nguyen_depaz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//example
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
